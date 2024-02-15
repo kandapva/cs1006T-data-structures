acf838ed43fb9b3f397ce9db4e0fd4567e1da69b
Kandappan <kandappanva@gmail.com>
2024-02-15 08:16:54 +0530

    added stl list example

---
+++ lists/stl_list.cpp
@@ -0,0 +1,39 @@
+// C++ program to demonstrate the implementation of List
+#include <iostream>
+#include <iterator>
+#include <list>
+using namespace std;
+ 
+// function for printing the elements in a list
+template<typename T>
+void print_list(list<T> g)
+{
+    list<T>::iterator it;
+    for (it = g.begin(); it != g.end(); ++it)
+        cout << '\t' << *it;
+    cout << endl;
+}
+ 
+// Driver Code
+int main()
+{
+ 
+    list<int> list1, list2;
+ 
+    for (int i = 0; i < 10; ++i) {
+        list1.push_back((i * 2 + 1));
+        list2.push_front((i * 3 -2));
+    }
+    cout << endl << "List 1 (gqlist1) is : ";
+    print_list(list1);
+
+    cout << endl << "List 2 (gqlist1) is : ";   
+    print_list(list2);
+ 
+    cout << "list1.front() : " << list1.front() << endl;
+    cout << "list1.back() : " << list1.back() << endl;
+ 
+
+ 
+    return 0;
+}
