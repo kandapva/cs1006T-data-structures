23c2c0416dd7425b9d2b33f1422671b33fa526db
Kandappan <kandappanva@gmail.com>
2024-02-25 12:08:25 +0530

    sample programs for queue model added

---
+++ queues/queue_array.cpp
@@ -0,0 +1,106 @@
+/*
+Array implementation of Stack
+*/
+#include <iostream>
+
+using namespace std;
+
+class Element
+{
+private:
+    double real, imag;
+    size_t key;
+
+public:
+    Element *next = NULL;
+    // The below is called a constructor
+    // It is used to initialise an object
+    Element(double a, double b, size_t c) : real(a), imag(b), key(c) {}
+    // The below is another constructor here we have overloaded
+    // It is used to create dummy object (as in case with list)
+    Element() {}
+    // The below is called a destructor
+    // It is used to clear space used by object once the object goes out of scope
+    ~Element() {}
+    // This is to print/iterate through the list
+    void print()
+    {
+        if (imag < 0)
+            cout << "(" << key << ") " << real << "-" << -1 * imag << "i" << endl;
+        else
+            cout << "(" << key << ") " << real << "+" << imag << "i" << endl;
+    }
+    bool isKey(int k)
+    {
+        if (key == k)
+            return true;
+        else
+            return false;
+    }
+};
+
+template <class T>
+class Queue
+{
+    int front = 0;
+    int rear = -1;
+    T *queue_array;
+    size_t queue_size_limit;
+public:
+    Queue(size_t N) : queue_size_limit(N)
+    { // N here is the maximum size of the array
+        queue_array = new T[queue_size_limit];
+    }
+    bool isQueueEmpty(){
+        if (rear==-1)
+            return true;
+        return false;
+    }
+    void EnQueue(T elem){
+        queue_array[++rear] = elem;
+    }
+    T DeQueue()
+    {
+        T tmp;
+        if(isQueueEmpty()){
+            std::cout << "illegal dequeue() operation" << std::endl;
+        }
+        else{
+            tmp = queue_array[front];
+            for(int i=0; i<rear; i++)
+                queue_array[i] = queue_array[i+1];
+            rear--;
+        }
+        return tmp;
+    }
+    T getFront(){
+        if (isQueueEmpty()){
+            cout << "Queue Empty.." << endl;
+        } 
+        return queue_array[front];
+    }
+    ~Queue(){
+    }
+};
+
+int main()
+{
+    Element *A = new Element(2, -10, 1);
+    Element *B = new Element(1.0, 5.6, 2);
+    Element *C = new Element(-3, 4, 3);
+    Queue<Element> q1(5);
+    q1.EnQueue(*A);
+    q1.EnQueue(*B);
+    q1.EnQueue(*C);
+    Element tmp;
+    tmp = q1.getFront();
+    tmp = q1.DeQueue();
+    tmp = q1.DeQueue();
+    q1.EnQueue(*B);
+    tmp = q1.getFront();
+    tmp.print();
+    delete A;
+    delete B;
+    delete C;
+    return 0;
+}
